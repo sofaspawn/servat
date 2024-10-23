1f37af495aef75634d879c41562c74a346ae2a1c
sofaspawn <h.m1nus.work@gmail.com>
2024-10-23 11:07:44 +0530

    tcp server

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module servat
 
 go 1.22.2
-
-require github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -1,24 +1,45 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"io"
 	"log"
-	"net/http"
-	"github.com/gorilla/websocket"
+	"net"
 )
-var upgrader = websocket.Upgrader{
-    ReadBufferSize: 1024,
-    WriteBufferSize: 1024,
-}
 
-func handler(w http.ResponseWriter, r *http.Request){
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err!=nil{
-        log.Println(err)
-        return
-    }
-    fmt.Println(conn.ReadMessage())
-}
+const PORT string = "localhost:6969";
 
 func main(){
+    l, err := net.Listen("tcp4", PORT);
+    if err!=nil{
+        log.Fatal("LISTEN: ", err);
+    }
+    defer l.Close();
+    for {
+        c, err := l.Accept();
+        if err!=nil{
+            log.Fatal("cannot accept connection on port: ", PORT);
+        }
+        go handleConnection(c);
+    }
+}
+
+func handleConnection(c net.Conn){
+    fmt.Printf("Serving on %s\n", c.RemoteAddr().String());
+    packet := make([]byte, 4096);
+    tmp := make([]byte, 4096);
+    defer c.Close();
+    for {
+        _, err := c.Read(tmp);
+        if err!=nil{
+            if err!=io.EOF{
+                log.Fatal("ERROR: ", err)
+            }
+            println("END OF FILE");
+            break
+        }
+        packet = append(packet, tmp...);
+    }
+    num, _ := c.Write(packet);
+    log.Printf("wrote back %d bytes, the payload is %s\n", num, string(packet));
 }
