32fae259dab4c745e4e7e48d6f0635efb33be442
sofaspawn <h.m1nus.work@gmail.com>
2024-11-07 14:24:24 +0530

    basic working done

--- client/client.go
+++ client/client.go
@@ -1,4 +1,32 @@
-package main;
+package main
+
+import (
+	"log"
+	"net"
+)
+
+const message = "imp message to be sent"
 
 func main(){
+    servAddr := "localhost:6969"
+    addr, err := net.ResolveTCPAddr("tcp", servAddr)
+
+    if err!=nil{
+        log.Fatalln(err)
+    }
+    conn, err := net.DialTCP("tcp", nil, addr)
+    if err!=nil{
+        log.Fatalln(err)
+    }
+    defer conn.Close()
+    _, err = conn.Write([]byte(message))
+    if err!=nil{
+        log.Fatalln(err)
+    }
+    reply := make([]byte, 1024)
+    _, err = conn.Read(reply)
+    if err!=nil{
+        log.Fatalln(err)
+    }
+    log.Printf("> %s\n", string(reply))
 }
