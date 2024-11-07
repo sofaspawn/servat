dfa35231bb16ffb0651d97aa32c9552af87bfafc
sofaspawn <h.m1nus.work@gmail.com>
2024-11-07 13:28:10 +0530

    refactoring

--- server/server.go
+++ server/server.go
@@ -1,44 +1,32 @@
-package main;
+package main
 
 import (
-	"fmt"
-	//"io"
 	"log"
 	"net"
+	"os"
 )
 
-const address string = "localhost:6969";
+const (
+    CONN_HOST = "localhost"
+    CONN_PORT = "6969"
+    CONN_TYPE = "tcp"
+)
 
-func main(){
-    chats := make([]byte, 10*1024);
-
-    l, err := net.Listen("tcp4", address);
-
+func main() {
+    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT);
     if err!=nil{
-        log.Fatal("LISTEN: ", err);
-    } else {
-        log.Printf("Listening on: http://%s\n", address);
+        log.Fatalln(err);
+        os.Exit(1);
     }
     defer l.Close();
-    for {
-        c, err := l.Accept();
+    for{
+        conn, err := l.Accept();
         if err!=nil{
-            log.Fatal("cannot accept connection on port: ", address);
+            log.Fatalln(err);
         }
-        go handleConnection(c, &chats);
+        buf := make([]byte, 1024);
+        conn.Read(buf);
     }
 }
 
-func handleConnection(c net.Conn, chats *[]byte){
-    fmt.Printf("Serving on %s\n", c.RemoteAddr().String());
-    packet := make([]byte, 4096);
-    //tmp := make([]byte, 4096);
-    _, err := c.Read(packet);
-    if err!=nil{
-        log.Fatal("ERROR: ", err);
-    }
-    defer c.Close();
-    *chats = append(*chats, packet...)
-    num, _ := c.Write(*chats);
-    log.Printf("wrote back %d bytes\n", num);
-}
+func handleRequest
