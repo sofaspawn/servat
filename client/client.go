64886fac4629dd81790ae2ca311199a560fc8383
sofaspawn <h.m1nus.work@gmail.com>
2024-11-14 22:46:43 +0530

    storing messages

--- client/client.go
+++ client/client.go
@@ -7,26 +7,25 @@ import (
 
 const message = "imp message to be sent"
 
-func main(){
-    servAddr := "localhost:6969"
-    addr, err := net.ResolveTCPAddr("tcp", servAddr)
-
-    if err!=nil{
-        log.Fatalln(err)
-    }
-    conn, err := net.DialTCP("tcp", nil, addr)
-    if err!=nil{
-        log.Fatalln(err)
-    }
-    defer conn.Close()
-    _, err = conn.Write([]byte(message))
-    if err!=nil{
-        log.Fatalln(err)
-    }
-    reply := make([]byte, 1024)
-    _, err = conn.Read(reply)
-    if err!=nil{
-        log.Fatalln(err)
-    }
-    log.Printf("> %s\n", string(reply))
+func main() {
+	servAddr := "localhost:6969"
+	addr, err := net.ResolveTCPAddr("tcp", servAddr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	reply := make([]byte, 4096)
+	_, err = conn.Read(reply)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("> %s\n", string(reply))
 }
--- server/server.go
+++ server/server.go
@@ -1,43 +1,57 @@
 package main
 
 import (
-    "log"
-    "net"
-    "os"
+	"log"
+	"net"
+	"os"
 )
 
 const (
-    CONN_HOST = "localhost"
-    CONN_PORT = "6969"
-    CONN_TYPE = "tcp"
+	CONN_HOST = "localhost"
+	CONN_PORT = "6969"
+	CONN_TYPE = "tcp"
 )
 
 func main() {
-    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-    if err != nil {
-        log.Fatalln("Error listening:", err.Error())
-        os.Exit(1)
-    }
-    defer l.Close()
-    log.Printf("Listening on %s:%s\n", CONN_HOST, CONN_PORT)
-    for {
-        conn, err := l.Accept()
-        if err != nil {
-            log.Fatalln("Error accepting: ", err.Error())
-        }
-        go handleRequest(conn)
-    }
+
+	backup := make([][]byte, 1024)
+
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		log.Fatalln("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+	log.Printf("Listening on %s:%s\n", CONN_HOST, CONN_PORT)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalln("Error accepting: ", err.Error())
+		}
+		go handleRequest(conn, &backup)
+	}
 }
 
-func handleRequest(conn net.Conn) {
-    log.Printf("handling connection: %s\n", conn.RemoteAddr().String())
-    buf := make([]byte, 1024)
-    _, err := conn.Read(buf)
-    if err != nil {
-        log.Fatalln("Error reading:", err.Error())
-    }
-    buf = append(buf, byte('\n'))
-    //conn.Write([]byte("Message received."))
-    conn.Write(buf)
-    conn.Close()
+func handleRequest(conn net.Conn, backup *[][]byte) {
+	log.Printf("handling connection: %s\n", conn.RemoteAddr().String())
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		log.Fatalln("Error reading:", err.Error())
+	}
+	//buf = append(buf, byte('\n'))
+	*backup = append(*backup, buf)
+	//conn.Write([]byte("Message received."))
+	ret := createResponse(backup)
+	log.Println(string(ret))
+	conn.Write(ret)
+	conn.Close()
+}
+
+func createResponse(backup *[][]byte) []byte {
+	ret := make([]byte, 2048)
+	for _, msg := range *backup {
+		ret = append(ret, msg...)
+	}
+	return ret
 }
