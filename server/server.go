6556ae6421730214d3adb5e8953e154b35366dd8
sofaspawn <h.m1nus.work@gmail.com>
2024-11-22 23:24:49 +0530

    server chat handling

--- server/server.go
+++ server/server.go
@@ -7,11 +7,28 @@ import (
 )
 
 func main() {
+
+    //chats := make([]string, 1024)
+
     router := mux.NewRouter()
 
     router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
         fmt.Fprint(w, "Hello World")
     })
 
+    router.HandleFunc("/chats", handleChat).Methods(http.MethodGet)
+
     http.ListenAndServe(":8000", router)
+}
+
+func getChats() []string {
+    chats := []string{"hello sofa!\n", "yes?\n", "how r u?\n"}
+    return chats
+}
+
+func handleChat(w http.ResponseWriter, r *http.Request){
+    chats := getChats()
+    for _, chat := range chats{
+        fmt.Fprint(w, chat)
+    }
 }
