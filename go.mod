846b2839df52fb20e682bd46792110b125121ccb
sofaspawn <h.m1nus.work@gmail.com>
2024-11-23 11:51:34 +0530

    sample ffmpeg encoding

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module servat
 
 go 1.22.2
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/aws/aws-sdk-go v1.55.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/u2takey/ffmpeg-go v0.5.0 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
+)
--- server/server.go
+++ server/server.go
@@ -1,8 +1,31 @@
 package main
 
-func main() {
-    a := 2
-    println(a)
+import (
+    "net/http"
+    "os/exec"
+    "log"
+)
+
+func streamVideo(w http.ResponseWriter, r *http.Request){
+    cmd := exec.Command("ffmpeg",
+    "-re",
+    "-i", "input.mp4",        // Input video file
+    "-c:v", "libx264",        // Encode video using H.264
+    "-preset", "ultrafast",   // Encoding speed/quality trade-off
+    "-f", "mpegts",           // Output format
+    "pipe:1")
+
+    cmd.Stdout = w
+    cmd.Stderr = log.Writer() // Log FFmpeg errors
+    w.Header().Set("Content-Type", "video/mp2t") // Set MIME type for video
+
+    if err := cmd.Run(); err != nil {
+        log.Println("Error running FFmpeg:", err)
+    }
 }
 
-
+func main(){
+    http.HandleFunc("/stream", streamVideo)
+    log.Println("Server started on :8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
