371bf7c1ca89538ece37691c1464d653b4683e6d
sofaspawn <h.m1nus.work@gmail.com>
2024-11-23 10:13:32 +0530

    fresh start

--- server/server.go
+++ server/server.go
@@ -1,38 +1,9 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
 )
 
-const port = ":8000"
 
 func main() {
-
-    //chats := make([]string, 1024)
-
-    router := mux.NewRouter()
-
-    fmt.Printf("serving on http://localhost%s\n", port)
-
-    router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprint(w, "Hello World")
-    })
-
-    router.HandleFunc("/chats", handleChat).Methods(http.MethodGet)
-
-    http.ListenAndServe(port, router)
 }
 
-func getChats() []string {
-    chats := []string{"hello sofa!\n", "yes?\n", "how r u?\n"}
-    return chats
-}
-
-func handleChat(w http.ResponseWriter, r *http.Request){
-    chats := getChats()
-    for _, chat := range chats{
-        fmt.Fprint(w, chat)
-    }
-}
